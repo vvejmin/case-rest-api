23ca7a2787ed07dfd798c86c160f4d03d82cc4ff
ejminvartoumian <vvejmin@githab.com>
2020-07-06 01:49:14 +0400

    case_study

--- main.go
+++ main.go
@@ -2,63 +2,61 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type promotion struct {
-	id              string `json:"id"`
-	price           string `json:price`
-	expiration_date string `json:"expiration_date"`
+// Model
+type Promotion struct {
+	ID             string `json:"id"`
+	Price          string `json:price`
+	ExpirationDate string `json:"expiration_date"`
 }
 
-type allPromotions []promotion
+// Init promotions var as a slice Promotion struct
+var promotions []Promotion
 
-var promotions = allPromotions{
-
-	{
-		id:              "172FFC14-D229-4C93-B06B-F48B8C095512",
-		price:           "9.68",
-		expiration_date: "2018-06-4 06:01:20",
-	},
-}
-
-func getOnePromotion(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for _, singlePromotion := range promotions {
-		if singlePromotion.id == eventID {
-			json.NewEncoder(w).Encode(singlePromotion)
+//Get Single Promotion
+func getPromotion(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Loop through promotions and find with id
+	for _, item := range promotions {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
 		}
 	}
+	json.NewEncoder(w).Encode(&Promotion{})
 }
 
 func updatePromotion(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-	var updatedPromotion promotion
-
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Not Found")
-	}
-	json.Unmarshal(reqBody, &updatedPromotion)
-
-	for i, singleEvent := range promotions {
-		if singleEvent.id == eventID {
-			promotions = append(promotions[:i], singleEvent)
-			json.NewEncoder(w).Encode(singleEvent)
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range promotions {
+		if item.ID == params["id"] {
+			promotions = append(promotions[:index], promotions[index+1:]...)
+			var promotion Promotion
+			_ = json.NewDecoder(r.Body).Decode(&promotion)
+			//
+			promotion.ID = params["id"]
+			promotions = append(promotions, promotion)
+			json.NewEncoder(w).Encode(promotion)
+			return
 		}
 	}
-
+	json.NewEncoder(w).Encode(promotions)
 }
 
 func main() {
+	//Init Router
 	r := mux.NewRouter().StrictSlash(true)
-	r.HandleFunc("/promotions/{id}", getOnePromotion).Methods("GET")
-	r.HandleFunc("/promotions/{id}", updatePromotion).Methods("PATCH")
+	// Mock Data - @todo - implement DB
+	promotions = append(promotions, Promotion{ID: "172FFC14-D229-4C93-B06B-F48B8C095512", Price: "9.68", ExpirationDate: "2018-06-4 06:01:20"})
+	//Roue Handlers / Endpoints
+	r.HandleFunc("/promotions/{id}", getPromotion).Methods("GET")
+	r.HandleFunc("/promotions/{id}", updatePromotion).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":1321", r))
 
 }
